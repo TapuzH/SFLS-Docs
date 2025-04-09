773f75cda1f7b371185a0803610cd98661cd1f3a
TapuzH <ta.puz@icloud.com>
2025-04-09 22:00:14 +0800

    update-20250409-2200

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,22 +19,30 @@ export default defineConfig({
 			],
 			title: '上外附中技术档案库',
 			social: [
-                     { icon: 'github', label: 'GitHub', href: 'https://github.com/TapuzH/SFLS-docs' },
-                     { icon: 'information', label: '学校官网', href: 'https://sfls.cn' }
-                     ],
-			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					icon: 'github',
+					label: 'GitHub',
+					href: 'https://github.com/TapuzH/SFLS-docs'
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+					icon: 'information',
+					label: '学校官网',
+					href: 'https://sfls.cn'
+				}
 			],
+			sidebar: [
+				{ label: '总则', link: "/main" },
+				{
+					label: '灯光与灯控台',
+					autogenerate: { directory: 'lighting' },
+				}
+			],
+			locales: {
+				root: {
+					label: '简体中文',
+					lang: 'zh-CN', // lang 是 root 语言必须的
+				},
+			},
 		}),
 	],
 });
